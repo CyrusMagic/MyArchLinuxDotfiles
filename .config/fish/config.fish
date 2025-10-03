f0bf8ede9336a76fa495826f90673d5e790d0df8
cyrus <CyrusMagic>
2025-10-03 23:54:15 +0800

    Add configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#starship init fish | source
+eval "$(starship init fish)"
+
+# Alias
+alias cla='claude'
+
+export CODEX_API_KEY='sk-ant-oat01-b84e237b832dc9fce97004e9efbb51ca3601e33866952eadeb3bac9255a9a297'
