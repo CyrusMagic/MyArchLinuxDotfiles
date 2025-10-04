cf7f6d62b526b248f56e8e48e882f89e2df31fb5
CyrusMagic <tintingoo23@gmail.com>
2025-10-04 20:37:06 +0800

    new arch machine dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,5 +7,11 @@ eval "$(starship init fish)"
 
 # Alias
 alias cla='claude'
+alias nv='nvim'
 
+# for codex
 export CODEX_API_KEY='sk-ant-oat01-b84e237b832dc9fce97004e9efbb51ca3601e33866952eadeb3bac9255a9a297'
+
+# for claude code
+export ANTHROPIC_BASE_URL=https://gaccode.com/claudecode
+export ANTHROPIC_API_KEY='sk-ant-oat01-b84e237b832dc9fce97004e9efbb51ca3601e33866952eadeb3bac9255a9a297'
