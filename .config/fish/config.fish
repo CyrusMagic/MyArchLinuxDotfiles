96fec18bf551372474da145f310ef16066d7f6e4
CyrusMagic <tintingoo23@gmail.com>
2025-10-05 14:40:49 +0800

    new

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,6 @@ export CODEX_API_KEY='sk-ant-oat01-b84e237b832dc9fce97004e9efbb51ca3601e33866952
 # for claude code
 export ANTHROPIC_BASE_URL=https://gaccode.com/claudecode
 export ANTHROPIC_API_KEY='sk-ant-oat01-b84e237b832dc9fce97004e9efbb51ca3601e33866952eadeb3bac9255a9a297'
+
+# for archlinuxcn
+#DEBUGINFOD_URLS="https://debuginfod.archlinux.org https://repo.archlinuxcn.org"
